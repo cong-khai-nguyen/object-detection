bf4a04b871aa62f63c0be0ef912d3b866bfca858
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-19 16:49:19 -0500

    read the image and template in

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import numpy as np
 import cv2
 
+img = cv2.resize(cv2.imread('media/soccer_practice.jpg', 0), (0,0), fx = 0.8, fy = 0.8)
+template = cv2.resize(cv2.imread('media/shoe.PNG', 0), (0,0), fx=0.8, fy=0.8)
+
+h,w = template.shape
+
