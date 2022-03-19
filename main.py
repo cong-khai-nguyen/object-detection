7b86e587c7e3327aca2ef45949f70189e93cb0be
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-19 16:55:34 -0500

    traverse through an array of methods to find the the method that works

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ template = cv2.resize(cv2.imread('media/shoe.PNG', 0), (0,0), fx=0.8, fy=0.8)
 
 h,w = template.shape
 
+methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
+          cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+
+for method in methods:
+    img2 = img.copy()
+
+    result = cv2.mathTemplate(img2, template, method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        location = min_loc
+    else:
+        location = max_loc
+
+    bottom_right = (location[0] + w, location[1] + h)
+
+
